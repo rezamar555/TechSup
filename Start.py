214c9bb1229c85e31cadf8b995e7cec4c0a1a3d5
rezamar555 <mauricio.reyes.sc@itszapopan.edu.mx>
2019-10-22 13:45:33 -0500

    Center Window and remove button test

--- Start.py
+++ Start.py
@@ -2,8 +2,7 @@ import sys
 
 from PyQt5.QtCore import QCoreApplication
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
-
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QDesktopWidget
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
@@ -11,14 +10,8 @@ if __name__ == '__main__':
     w = QWidget()
     w.setWindowTitle('Inicial')
 
-    btn = QPushButton('Este es un Button', w)
-#    btn.setToolTip('This is a <b>QPushButton</b> widget') # As Reference
-    btn.move(150, 50)
-
-    btn.clicked.connect(QCoreApplication.instance().quit)
-
+    w.move(QDesktopWidget().availableGeometry().center() - w.frameGeometry().center())    
     w.resize(500, 500)
     w.show()
 
     sys.exit(app.exec_())
-
