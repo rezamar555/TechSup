a23acfaeda435109d2d215aed7bc0a35d4dabf85
rezamar555 <mauricio.reyes.sc@itszapopan.edu.mx>
2019-10-24 01:07:25 -0500

    Added new QPannel to draw

--- Start.py
+++ Start.py
@@ -1,5 +1,6 @@
 import sys
 #Project name and libraries
+from PyQt5 import uic, QtGui
 from PyQt5.QtCore import QCoreApplication, Qt
 from PyQt5.QtGui import QIcon, QPen, QBrush, QPainter
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QDesktopWidget, QMainWindow
@@ -12,11 +13,6 @@ class Window(QMainWindow):
         self.title = "TechSup"
         self.InitWindow()
 
-        '''def InitWindow(self):            ###Draw insert of lines
-            self.setWindowTitle(self.title)
-            self.setGeometry(300, 300, 280, 270)
-            self.show()'''
-
     def InitWindow(self):
         self.setWindowTitle(self.title)
         self.showMaximized()
@@ -25,40 +21,8 @@ class Window(QMainWindow):
     def paintEvent(self, e):
         painter = QPainter(self)
         painter.setPen(QPen(Qt.black, 5, Qt.SolidLine))
-        #self.drawLines(painter) ##black board
-        painter.setBrush(QBrush(Qt.black))
+        painter.setBrush(QBrush(Qt.black, Qt.Dense1Pattern))
         painter.drawRect(20,20,900,500)
-        '''qp = QPainter()
-        qp.begin(self)
-        self.drawLines(qp)
-        qp.end()'''
-
-    '''def drawLines(self, qp):                 ###Draw Functions in QWidget
-        pen = QPen(Qt.black, 2, Qt.SolidLine)
-
-        qp.setPen(pen)
-        qp.drawLine(20, 40, 250, 40)
-
-        pen.setStyle(Qt.DashLine)
-        qp.setPen(pen)
-        qp.drawLine(20, 80, 250, 80)
-
-        pen.setStyle(Qt.DashDotLine)
-        qp.setPen(pen)
-        qp.drawLine(20, 120, 250, 120)
-
-        pen.setStyle(Qt.DotLine)
-        qp.setPen(pen)
-        qp.drawLine(20, 160, 250, 160)
-
-        pen.setStyle(Qt.DashDotDotLine)
-        qp.setPen(pen)
-        qp.drawLine(20, 200, 250, 200)
-
-        pen.setStyle(Qt.CustomDashLine)
-        pen.setDashPattern([1, 4, 5, 4])
-        qp.setPen(pen)
-        qp.drawLine(20, 240, 250, 240)'''
 
 
 App = QApplication(sys.argv)
