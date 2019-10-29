c3b2be33d4b198ee61ff4f5517f3395e78f120a8
rezamar555 <mauricio.reyes.sc@itszapopan.edu.mx>
2019-10-28 23:09:56 -0600

    Added stablished drawing table.

--- Start.py
+++ Start.py
@@ -1,18 +1,19 @@
 import sys
-#Project name and libraries
-from PyQt5 import uic, QtGui, QtCore, QtWidgets
-from PyQt5.QtCore import QCoreApplication, Qt
-from PyQt5.QtGui import QIcon, QPen, QBrush, QPainter
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QDesktopWidget, QMainWindow
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtCore import Qt
 
-#class Window(QMainWindow):
 class Canvas(QtWidgets.QLabel):
 
     def __init__(self):
         super().__init__()
+        pixmap = QtGui.QPixmap(600, 300)
+        self.setPixmap(pixmap)
 
-        #pixmap = QtGui.QPixmap(600,300)
-        #self.setPixmap(pixmap)
+        self.last_x, self.last_y = None, None
+        self.pen_color = QtGui.QColor('#000000')
+
+    def set_pen_color(self, c):
+        self.pen_color = QtGui.QColor(c)
 
     def mouseMoveEvent(self, e):
         if self.last_x is None: # First event.
@@ -20,25 +21,22 @@ class Canvas(QtWidgets.QLabel):
             self.last_y = e.y()
             return # Ignore the first time.
 
-            #def paintEvent(self, e):
-            #    painter = QPainter(self)
-            painter = QtGui.QPainter(self.pixmap())
-            p = painter.pen()
-            p.setColor(self.pen_color)
-            painter.setPen(p)
-            #painter.setPen(QPen(Qt.black, 5, Qt.SolidLine))
-            painter.drawLine(self.last_x, self.last_y, e.x(), e.y())
-            painter.setBrush(QBrush(Qt.black, Qt.Dense1Pattern))
-            painter.drawRect(20,20,900,500)
-            painter.end()
-            self.update()
+        painter = QtGui.QPainter(self.pixmap())
+        p = painter.pen()
+        p.setWidth(4)
+        p.setColor(self.pen_color)
+        painter.setPen(p)
+        painter.drawLine(self.last_x, self.last_y, e.x(), e.y())
+        painter.end()
+        self.update()
 
-            self.last_x = e.x()
-            self.last_y = e.y()
+        # Update the origin for next time.
+        self.last_x = e.x()
+        self.last_y = e.y()
 
-            def mouseReleaseEvent(self, e):
-                self.last_x = None
-                self.last_y = None
+    def mouseReleaseEvent(self, e):
+        self.last_x = None
+        self.last_y = None
 
 COLORS = [
 # 17 undertones https://lospec.com/palette-list/17undertones
@@ -46,6 +44,7 @@ COLORS = [
 '#458352', '#dcd37b', '#fffee5', '#ffd035', '#cc9245', '#a15c3e', '#a42f3b',
 '#f45b7a', '#c24998', '#81588d', '#bcb0c2', '#ffffff',
 ]
+
 
 class QPaletteButton(QtWidgets.QPushButton):
 
@@ -60,14 +59,9 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.title = "TechSup"
         self.canvas = Canvas()
-        self.InitWindow()
 
-    def InitWindow(self):
-        self.setWindowTitle(self.title)
-        self.showMaximized()
-
+        self.setWindowTitle("TechSup")
         w = QtWidgets.QWidget()
         l = QtWidgets.QVBoxLayout()
         w.setLayout(l)
@@ -79,22 +73,12 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.setCentralWidget(w)
 
-    def paintEvent(self, e):
-        '''painter = QPainter(self)
-        painter = setPen(QPen(Qt.black, 5, Qt.SolidLine))
-        painter.setBrush(QBrush(Qt.black, Qt.Dense1Pattern))
-        painter.drawRect(20,20,900,500)'''
-        painter = QPainter(self) ##temp
-        painter.setPen(QPen(Qt.black, 5, Qt.SolidLine))
-        painter.setBrush(QBrush(Qt.black, Qt.Dense1Pattern))
-        painter.drawRect(20,20,900,500) ##temp
-
-
     def add_palette_buttons(self, layout):
         for c in COLORS:
             b = QPaletteButton(c)
             b.pressed.connect(lambda c=c: self.canvas.set_pen_color(c))
             layout.addWidget(b)
+
 
 app = QtWidgets.QApplication(sys.argv)
 window = MainWindow()
